495c173e00745942da67469346c80871d6dbafbe
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2023-09-19 21:27:32 +0200

    fix: tweak of og endpoint setup

--- src/app/api/og/route.ts
+++ src/app/api/og/route.ts
@@ -1,5 +1,5 @@
-import { getOpenGraphImage } from "~/server/og"
+import { getOpenGraphImage } from "~/server/og";
 
-export const runtime = "edge"
+export const runtime = "edge";
 
-export const GET = getOpenGraphImage()
+export const GET = getOpenGraphImage;
